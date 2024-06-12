847c8894fcd5ad0fe787f7a39b1a65e8003a2024
Bashar V I <basharvismail@gmail.com>
2024-06-12 22:37:05 +0400

    12th June 2024- 2nd commit

--- easy.js
+++ easy.js
@@ -148,3 +148,14 @@ const relativeSortArray = (arr1, arr2) => {
 
   return arr1;
 };
+
+// 2037. Minimum Number of Moves to Seat Everyone
+const minMovesToSeat = (seats, students) => {
+  seats.sort((a, b) => a - b);
+  students.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < seats.length; i++) {
+    sum += Math.abs(seats[i] - students[i]);
+  }
+  return sum;
+};
