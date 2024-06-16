8402ae3c2d72f01d025bc4110c3f26dd781e9f4a
Bashar V I <basharvismail@gmail.com>
2024-06-16 09:21:55 +0400

    16th June 2024

--- hard.js
+++ hard.js
@@ -24,3 +24,23 @@ const findMaximizedCapital = (k, w, profits, capital) => {
   }
   return w;
 };
+
+// 330. patching Array
+
+const minPatches = (nums, n) => {
+  let patches = 0;
+  let miss = 1;
+  let i = 0;
+
+  while (miss <= n) {
+    if (i < nums.length && nums[i] <= miss) {
+      miss += nums[i];
+      i++;
+    } else {
+      miss += miss;
+      patches++;
+    }
+  }
+
+  return patches;
+};
