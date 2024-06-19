283a02b785e2ca1ec52e9a303068c00697ae570f
Bashar V I <basharvismail@gmail.com>
2024-06-19 13:22:55 +0400

    19th June 2024

--- medium.js
+++ medium.js
@@ -208,6 +208,48 @@ const subarrayDivByK = (nums, k) => {
   return count;
 };
 
+// 1482. Minimum Number of Days to make m Bouquets
+
+const minDays = (bloomDay, m, k) => {
+  if (m * k > bloomDay.length) {
+    return -1;
+  }
+
+  const canMakeBouquets = (days) => {
+    let bouquets = 0;
+    let flowers = 0;
+
+    for (let bloom of bloomDay) {
+      if (bloom <= days) {
+        flowers++;
+        if (flowers == k) {
+          bouquets++;
+          flowers = 0;
+        }
+      } else {
+        flowers = 0;
+      }
+    }
+
+    return bouquets >= m;
+  };
+
+  let left = Math.min(...bloomDay);
+  let right = Math.max(...bloomDay);
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (canMakeBouquets(mid)) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
+
 // 2486. Append Characters to strinng to make subsequence
 
 const appendCharacters = (s, t) => {
