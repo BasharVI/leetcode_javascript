9d8b88decaffaaad31066483bb29e3206d9d779a
Bashar V I <basharvismail@gmail.com>
2024-06-20 13:38:49 +0400

    20th June 2024

--- medium.js
+++ medium.js
@@ -250,6 +250,45 @@ const minDays = (bloomDay, m, k) => {
   return left;
 };
 
+// 1552. Magnetic force between two balls
+const canPlaceBalls = (position, m, minDistance) => {
+  let count = 1;
+  let lastPosition = position[0];
+
+  for (let i = 1; i < position.length; i++) {
+    if (position[i] - lastPosition >= minDistance) {
+      count++;
+      lastPosition = position[i];
+      if (count >= m) return true;
+    }
+  }
+
+  return false;
+};
+
+const maxDistance = (position, m) => {
+  position.sort((a, b) => a - b);
+
+  let left = 1;
+  let right = position[position.length - 1] - position[0];
+  let result = 0;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (canPlaceBalls(position, m, mid)) {
+      result = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(maxDistance([1, 2, 3, 4, 7], 3));
+
 // 2486. Append Characters to strinng to make subsequence
 
 const appendCharacters = (s, t) => {
