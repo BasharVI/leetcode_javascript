57d6ae4a634c4a99467d865631e7ea992a58e482
Bashar V I <basharvismail@gmail.com>
2024-06-23 16:31:18 +0400

    23rd June 2024

--- medium.js
+++ medium.js
@@ -267,6 +267,49 @@ const numberOfSubarrays = (nums, k) => {
 
   return count;
 };
+
+// 1438. Longest Continuous Subarray With Absolute Diff less than or Equal to Limit
+
+const longestSubarray = (nums, limit) => {
+  let left = 0;
+  let maxLen = 0;
+
+  let minDeque = [];
+  let maxDeque = [];
+
+  for (let right = 0; right < nums.length; right++) {
+    while (
+      minDeque.length &&
+      nums[minDeque[minDeque.length - 1]] > nums[right]
+    ) {
+      minDeque.pop();
+    }
+    minDeque.push(right);
+
+    while (
+      maxDeque.length &&
+      nums[maxDeque[maxDeque.length - 1]] < nums[right]
+    ) {
+      maxDeque.pop();
+    }
+    maxDeque.push(right);
+
+    while (nums[maxDeque[0]] - nums[minDeque[0]] > limit) {
+      left++;
+      if (minDeque[0] < left) {
+        minDeque.shift();
+      }
+      if (maxDeque[0] < left) {
+        maxDeque.shift();
+      }
+    }
+
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+
+  return maxLen;
+};
+
 // 1482. Minimum Number of Days to make m Bouquets
 
 const minDays = (bloomDay, m, k) => {
