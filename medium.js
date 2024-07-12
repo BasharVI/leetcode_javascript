03df54414c180cd273f17f67169f5cb22a9b61c4
Bashar V I <basharvismail@gmail.com>
2024-07-12 11:07:02 +0400

    12th July 2024

--- medium.js
+++ medium.js
@@ -506,6 +506,41 @@ const averageWaitingTime = (customers) => {
   return totalWaitingTime / customers.length;
 };
 
+// 1717. Maximum Score from removing Substring
+
+const maximumGain = (s, x, y) => {
+  let aCount = 0;
+  let bCount = 0;
+  let lesser = Math.min(x, y);
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+    if (c > "b") {
+      result += Math.min(aCount, bCount) * lesser;
+      aCount = 0;
+      bCount = 0;
+    } else if (c === "a") {
+      if (x < y && bCount > 0) {
+        bCount--;
+        result += y;
+      } else {
+        aCount++;
+      }
+    } else if (c === "b") {
+      if (x > y && aCount > 0) {
+        aCount--;
+        result += x;
+      } else {
+        bCount++;
+      }
+    }
+  }
+
+  result += Math.min(aCount, bCount) * lesser;
+  return result;
+};
+
 // 1823. Find the Winner of the circular game
 
 const findTheWinner = (n, k) => {
